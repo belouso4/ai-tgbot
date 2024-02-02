8d5891b6fb4ea7f273c33695aa6902c93d348886
belouso4 <kibelousov495@proton.me>
2024-02-02 14:35:22 +0700

    added prod functionality

--- src/main.js
+++ src/main.js
@@ -9,13 +9,15 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
+const BOT_TOKEN = process.env.BOT_TOKEN || '';
+const ENVIRONMENT = process.env.NODE_ENV || '';
 
 import { ogg } from './ogg.js'
 import { removeFile } from './utils.js'
 import { openai } from './openai.js'
 import {processTextToChat, INITIAL_SESSION, SELECT_LANGUAGE} from './logic.js'
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(BOT_TOKEN)
 
 const i18n = new TelegrafI18n({
   directory: path.resolve(__dirname, 'locales'),
@@ -100,7 +102,9 @@ bot.on(message('text'), async (ctx) => {
     }
 })
 
-bot.launch()
-
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+//prod mode (Vercel)
+export const startVercel = async (req, res) => {
+  await production(req, res, bot);
+};
+//dev mode
+ENVIRONMENT !== 'production' && development(bot);
